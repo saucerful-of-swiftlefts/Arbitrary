1b3d1415782d6063747d43f0f0c601ab6e9a9586
月光虹 <iris@random.town>
2021-02-05 23:09:33 +0100

    Implement Type Erasure

--- Sources/Arbitrary/AnyGenerator.swift
+++ Sources/Arbitrary/AnyGenerator.swift
@@ -1,11 +1,14 @@
 public struct AnyGenerator<Element>: GenerativeSequence {
 
-	public init<G: GenerativeSequence>(_ generator: G) where G.Element == Element {
+	private var source: Generator.Closure<Element>
 
+	public init<G: GenerativeSequence>(_ generator: G) where G.Element == Element {
+		var input = generator
+		source = .init { input.next() }
 	}
 
 	public func next() -> Element? {
-		nil
+		source.next()
 	}
 }
 
