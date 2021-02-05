7cb6191b32af9e1567ed78dd349239aa1f2e8600
月光虹 <iris@random.town>
2021-02-05 22:48:25 +0100

    Test that Sequential Generator Indeed Produces Values from Input Sequence

---
+++ Tests/ArbitraryTests/SequentialGeneratorTests.swift
@@ -0,0 +1,15 @@
+@testable import Arbitrary
+
+import XCTest
+
+final class SequentialGeneratorTests: XCTestCase {
+
+	func testGeneration() {
+		for _ in 1...100 {
+			let input = (0...Int.random(in: 0...100))
+			let generator = Generator.Sequential<Int>(from: input)
+
+			XCTAssertEqual(Array(input), Array(generator))
+		}
+	}
+}
