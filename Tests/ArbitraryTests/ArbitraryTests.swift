06ea852d63126bb0a795ef1f464697926eb0cb3a
月光虹 <iris@random.town>
2021-02-05 23:26:34 +0100

    Test that Arbitrary Booleans are Correctly Generated

--- Tests/ArbitraryTests/ArbitraryTests.swift
+++ Tests/ArbitraryTests/ArbitraryTests.swift
@@ -4,4 +4,14 @@ import XCTest
 
 final class ArbitraryTests: XCTestCase {
 
+	func testBool() {
+		for _ in 0...25 {
+			let generations = 200
+			let generator = Bool.arbitrary
+			let values: [Bool?] = Array(generator.prefix(generations))
+			let counts = values.reduce(into: [Bool?: Int]()) { $0[$1, default: 0] += 1 }
+
+			XCTAssertEqual(counts[.none] ?? 0, 0, "not every generation produced a value")
+		}
+	}
 }
