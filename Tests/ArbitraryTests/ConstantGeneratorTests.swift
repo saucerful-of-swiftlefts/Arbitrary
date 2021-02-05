91d1f55cf7c95eb6c83b40f98e4ffafa6dbfdd7b
月光虹 <iris@random.town>
2021-02-05 22:34:03 +0100

    Test that Constant Generator Always Produces the Same Next Element

---
+++ Tests/ArbitraryTests/ConstantGeneratorTests.swift
@@ -0,0 +1,15 @@
+@testable import Arbitrary
+
+import XCTest
+
+final class ConstantGeneratorTests: XCTestCase {
+
+	func testGeneration() {
+		let value = Int.random(in: 0...9999)
+		let generator = Generator.Constant(value)
+
+		for _ in 1...100 {
+			XCTAssertEqual(value, generator.next())
+		}
+	}
+}
