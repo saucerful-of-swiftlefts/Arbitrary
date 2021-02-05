58c02b2e5edf0a3ac1262c36e5606a4cd944b00a
月光虹 <iris@random.town>
2021-02-06 00:16:45 +0100

    Implement Cyclic Generation from Source Generator

--- Sources/Arbitrary/Generators/Cyclic.swift
+++ Sources/Arbitrary/Generators/Cyclic.swift
@@ -2,11 +2,31 @@ public extension Generator {
 	struct Cyclic<Element, Source: GenerativeSequence>: GenerativeSequence
 	where Source.Element == Element {
 
+		private var iterator: IndexingIterator<[Element]>? = nil
+
+		private var elements: [Element] = []
+
+		private var source: Source
+
 		public init(_ generator: Source) {
+			source = generator
 		}
 
-		public func next() -> Element? {
-			nil
+		public mutating func next() -> Element? {
+			if let value = source.next() {
+				elements.append(value)
+				return value
+			}
+
+			while !elements.isEmpty {
+				if let value = iterator?.next() {
+					return value
+				} else {
+					self.iterator = elements.makeIterator()
+				}
+			}
+
+			return nil
 		}
 	}
 }
