7ddd65cb980ad23d03015b6718c738eb9e612062
月光虹 <iris@random.town>
2021-02-06 03:07:20 +0100

    Randomized the Amount of Test Generations for Arbitrary Booleans
    Skip expecting the minimum amount of entropy if there was not enough generations.

--- Tests/ArbitraryTests/ArbitraryTests.swift
+++ Tests/ArbitraryTests/ArbitraryTests.swift
@@ -6,13 +6,16 @@ final class ArbitraryTests: XCTestCase {
 
 	func testBool() {
 		for _ in 0...25 {
-			let generations = 200
+			let generations = Int.random(in: 0...500)
 			let generator = Bool.arbitrary
 			let values: [Bool?] = Array(generator.prefix(generations))
 			let counts = values.reduce(into: [Bool?: Int]()) { $0[$1, default: 0] += 1 }
 			let all: [Bool] = values.filter { $0 != nil }.map { $0! }
 
 			XCTAssertEqual(counts[.none] ?? 0, 0, "not every generation produced a value")
+
+			guard generations > 75 else { continue }
+
 			XCTAssertGreaterThan(all.entropy, 1.25, "distribution is not random enough")
 		}
 	}
