95798c353fe56f356c817692f58fe074a1a2687d
月光虹 <iris@random.town>
2021-02-06 02:35:59 +0100

    Make Sure that Arbitrary Booleans are Well-distributed

--- Tests/ArbitraryTests/ArbitraryTests.swift
+++ Tests/ArbitraryTests/ArbitraryTests.swift
@@ -10,8 +10,10 @@ final class ArbitraryTests: XCTestCase {
 			let generator = Bool.arbitrary
 			let values: [Bool?] = Array(generator.prefix(generations))
 			let counts = values.reduce(into: [Bool?: Int]()) { $0[$1, default: 0] += 1 }
+			let all: [Bool] = values.filter { $0 != nil }.map { $0! }
 
 			XCTAssertEqual(counts[.none] ?? 0, 0, "not every generation produced a value")
+			XCTAssertGreaterThan(all.entropy, 1.25, "distribution is not random enough")
 		}
 	}
 }
